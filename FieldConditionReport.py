036586c7c49b11d4d8d3cf49abfd6ef172652411
Josh Leong <leong.jo@gmail.com>
2022-02-03 12:30:27 -0700

    finished login, now attempting FCR Click

--- FieldConditionReport.py
+++ FieldConditionReport.py
@@ -1,25 +1,50 @@
-"""
-Field Condition Report Automated Entry
-Project runs Selenium browser
+# ***
+# Field Condition Report Automated Entry
+# Project runs Selenium browser
+# Open URL https://opssuitemain.swacorp.com/station-management/conditions
+# C:\Users\e86742\Desktop\Projects\pythonprojects\FieldConditionReport>
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
 
-Open URL https://opssuitemain.swacorp.com/station-management/conditions
-"""
-while True:
-    print("Please Enter the Departure Runways")
+
+def opssuite_login():
+    driver.get("https://opssuitemain.swacorp.com/station-management/conditions")
+    driver.maximize_window()
+    driver.find_element_by_id(
+        "ContentPlaceHolder1_MFALoginControl1_UserIDView_txtUserid_UiInput").send_keys("E86742")
+    driver.find_element_by_id(
+        "ContentPlaceHolder1_MFALoginControl1_UserIDView_tbxPassword_UiInput").send_keys("Kotaemasu.8")
+    driver.find_element_by_class_name("btn--login").click()
 
 
-fcrURL = 'https://opssuitemain.swacorp.com/station-management/conditions'
+opssuite_login()
+
+def clickFCR():
+    driver.find_element("//*[contains(text(),'Create FCR')]").click()
+
+clickFCR()
+   
 
 
+
+
+# finalDepartureRunways = []
+# ArrivalRunways = []
+# while True:
+#     x = str(input("Please Enter a Departure Runway"))
+#     y = str(input("Please Enter a Arrival Runways"))
+#     break
+# #locate Braking Action
+# fcrURL = 'https://opssuitemain.swacorp.com/station-management/conditions'
 # Click Create FCR
-
 """
 Variables for Runways
-
 """
-
 # Ensure Update radio button is selected
-
 # Find Section
 # Click within Ramp Section
 """
@@ -28,12 +53,7 @@ Condition
 Reported By
 Observation Time 
 Remarks
-
 """
-
 # Click Add Runway Button
-
-
 # Taxiway
-
 # Format Time units
