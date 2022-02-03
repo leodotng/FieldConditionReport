fbd8eaa0bc2e1c798a1f12f6e255251184f277a4
Josh Leong <leong.jo@gmail.com>
2022-02-03 06:05:07 -0700

    initial commit

---
+++ FieldConditionReport.js
@@ -0,0 +1,38 @@
+/*
+Field Condition Report Automated Entry
+Project runs Selenium browser
+
+Open URL https://opssuitemain.swacorp.com/station-management/conditions
+*/
+
+let runwayList = []
+let fcrURL = 'https://opssuitemain.swacorp.com/station-management/conditions';
+
+
+// Click Create FCR
+
+/*
+Variables for Runways
+
+*/
+
+// Ensure Update radio button is selected
+
+// Find Section
+// Click within Ramp Section
+/*
+Braking Action Dropdown
+Condition
+Reported By
+Observation Time 
+Remarks
+*/
+
+// Click Add Runway Button
+
+
+// Taxiway
+
+// Format Time units
+
+// Clickable items Create FCR Button 
---
+++ FieldConditionReport.py
@@ -0,0 +1,39 @@
+"""
+Field Condition Report Automated Entry
+Project runs Selenium browser
+
+Open URL https://opssuitemain.swacorp.com/station-management/conditions
+"""
+while True:
+    print("Please Enter the Departure Runways")
+
+
+fcrURL = 'https://opssuitemain.swacorp.com/station-management/conditions'
+
+
+# Click Create FCR
+
+"""
+Variables for Runways
+
+"""
+
+# Ensure Update radio button is selected
+
+# Find Section
+# Click within Ramp Section
+"""
+Braking Action Dropdown
+Condition
+Reported By
+Observation Time 
+Remarks
+
+"""
+
+# Click Add Runway Button
+
+
+# Taxiway
+
+# Format Time units
